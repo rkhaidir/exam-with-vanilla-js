dc568062c586ca3af8f9c7c5477ec1080fd26a2a
rkhaidir <rkhaidir085@gmail.com>
2020-03-18 22:15:17 +0800

    update score page

--- hasil.js
+++ hasil.js
@@ -8,9 +8,20 @@ quizResults.results = []
 
 let result = {}
 
+let note
+let score = (parseInt(correctAnswer) / parseInt(questionLength)) * 100
+if(score >= 65) {
+    note = "Lulus"
+} else {
+    note = "Tidak Lulus"
+}
+score = score < 100 ? score.toFixed(2) : score
+
 result.nameUser = nameUser
 result.correctAnswer = correctAnswer
 result.wrongAnswer = wrongAnswer
+result.scoreUser = score
+result.noteUser = note
 
 if(localStorage && localStorage.getItem('quizMaster')) {
     quizResults = JSON.parse(localStorage.getItem('quizMaster'))
@@ -37,43 +48,45 @@ function clearResults() {
     sessionStorage.setItem("name", "")
 }
 
-function resultsHistory(testNumber, nameUser, correctAnswer, wrongAnswer) {
+function resultsHistory(testNumber, nameUser, correctAnswer, wrongAnswer, scoreUser, noteUser) {
     let resultsContainer = document.querySelector(".resultsContainer")
 
-    let note
-    let score = (parseInt(correctAnswer) / parseInt(questionLength)) * 100
-
-    if(score >= 65) {
-        note = "Lulus"
-    } else {
-        note = "Tidak Lulus"
-    }
-    score = score < 100 ? score.toFixed(2) : score
     let tr = document.createElement('tr')
     let html = `
         <td>${testNumber}</td>
         <td>${nameUser}</td>
         <td>${correctAnswer}</td>
         <td>${wrongAnswer}</td>
-        <td>${score}</td>
-        <td>${note}</td>
+        <td>${scoreUser}</td>
+        <td>${noteUser}</td>
     `
     tr.innerHTML = html
     resultsContainer.appendChild(tr)
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+function newResults(numCorrect, numWrong, score) {
 
+    let newScore = document.querySelector("#new-score")
+    let wrongCorrect = document.querySelector("#wrong-correct")
+
+    newScore.innerHTML = `Score yang anda peroleh<br/>${score}`
+    wrongCorrect.innerHTML =`Jawaban Benar: <b>${numCorrect}</b>, Jawaban Salah: <b>${numWrong}</b><hr>`
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    let quizMaster = quizResults["results"].length
     let testNumber = 1
 
-    for(let i=0; i<quizResults["results"].length; i++) {
+    for(let i=0; i<quizMaster; i++) {
         if(quizResults["results"][i].correctAnswer != "" && quizResults["results"][i].wrongAnswer != "") {
-            resultsHistory(testNumber, quizResults["results"][i].nameUser, quizResults["results"][i].correctAnswer, quizResults["results"][i].wrongAnswer)
+            resultsHistory(testNumber, quizResults["results"][i].nameUser, quizResults["results"][i].correctAnswer, quizResults["results"][i].wrongAnswer, quizResults["results"][i].scoreUser, quizResults["results"][i].noteUser)
         }
         testNumber++
     }
 
-    console.log(quizResults["results"].length)
+    if(result.nameUser != ""){
+        newResults(quizResults["results"][quizMaster-1].correctAnswer, quizResults["results"][quizMaster-1].wrongAnswer, quizResults["results"][quizMaster-1].scoreUser)
+    }
 
     let tryAgain = document.querySelector("#try-again")
     tryAgain.addEventListener("click", function(){
