58eddc94a1858e14e66d9c4d902e53e47cf90080
rkhaidir <rkhaidir085@gmail.com>
2020-08-27 20:39:41 +0800

    XA

---
+++ xa-rpl/hasil.js
@@ -0,0 +1,56 @@
+let correctAnswer = localStorage.getItem("correct")
+let wrongAnswer = localStorage.getItem("wrong")
+let answerUser = localStorage.getItem("userAnswer")
+let nameUser = localStorage.getItem("name")
+let questionLength = localStorage.getItem("questionLength")
+
+let quizResults = {}
+quizResults.results = []
+
+let result = {}
+
+let note
+let score = (parseInt(correctAnswer) / parseInt(questionLength)) * 100
+if(score >= 65) {
+    note = "Lulus"
+} else {
+    note = "Tidak Lulus"
+}
+score = score < 100 ? score.toFixed(2) : score
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBDiqde_YlNmd4i9WmnDLO4XclbWTwd76E",
+    authDomain: "exam-application-c4943.firebaseapp.com",
+    databaseURL: "https://exam-application-c4943.firebaseio.com",
+    projectId: "exam-application-c4943",
+    storageBucket: "exam-application-c4943.appspot.com",
+    messagingSenderId: "150768748127",
+    appId: "1:150768748127:web:c0fa951ac23407cae97dea",
+    measurementId: "G-66290TJEHE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let d = new Date();
+const data = {
+    useName         : nameUser,
+    correctAnswer   : correctAnswer,
+    wrongAnswer     : wrongAnswer,
+    answerUser      : answerUser,
+    score           : score,
+    date            : `${d.getDate()}-${d.getMonth()}-${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`
+}
+var database = firebase.database();
+database.ref(`score/10A_RPL`).push(data);
+removeLocal()
+
+function removeLocal() {
+    localStorage.removeItem("correct")
+    localStorage.removeItem("wrong")
+    localStorage.removeItem("userAnswer")
+    localStorage.removeItem("name")
+    localStorage.removeItem("questionLength")
+}
---
+++ xa-rpl/script.js
@@ -0,0 +1,182 @@
+const myQuestions = [
+    {
+        question: "Sistem bilangan yang hanya terdiri dari 0 dan 1 disebut....",
+        answers: {
+            a: "Heksadesimal",
+            b: "Biner",
+            c: "Oktal",
+            d: "Desimal",
+            e: "Binary code"
+        },
+        code : 1,
+        correctAnswer: "b"
+    },
+    {
+        question: "Sistem bilangan yang memiliki radiks atau basis 10 disebut….",
+        answers: {
+            a: "Heksadesimal",
+            b: "Biner",
+            c: "Oktal",
+            d: "Desimal",
+            e: "Binary code"
+        },
+        code : 2,
+        correctAnswer: "d"
+    },
+    {
+        question: "Sistem bilangan yang memiliki radiks atau basis 8 disebut.…",
+        answers: {
+            a: "Heksadesimal",
+            b: "Biner",
+            c: "Oktal",
+            d: "Desimal",
+            e: "Binary code"
+        },
+        code : 3,
+        correctAnswer: "c"
+    },
+    {
+        question: "<img src='/img/4.jpg' class='img-fluid'><br>Diantara pasangan tersebut, manakah yang sesuai antara jenis bilangan dan contohnya....",
+        answers: {
+            a: "1 dan 2",
+            b: "2 dan 3",
+            c: "2 dan 4",
+            d: "1 dan 4",
+            e: "3 dan 4"
+        },
+        code : 4,
+        correctAnswer: "b"
+    },
+    {
+        question: "Hasil konversi dari bilangan desimal 929 ke biner adalah...",
+        answers: {
+            a: "0100110001",
+            b: "1110100001",
+            c: "1110001111",
+            d: "0011111100",
+            e: "1111111111"
+        },
+        code : 5,
+        correctAnswer: "b"
+    },
+    {
+        question: "Hasil konversi dari bilangan biner 011 ke oktal adalah....",
+        answers: {
+            a: "1",
+            b: "2",
+            c: "3",
+            d: "4",
+            e: "7"
+        },
+        code : 6,
+        correctAnswer: "c"
+    },
+    {
+        question: "Hasil konversi dari bilangan biner 11110110 ke heksadesimal adalah....",
+        answers: {
+            a: "F1",
+            b: "F3",
+            c: "F4",
+            d: "F6",
+            e: "F7"
+        },
+        code : 7,
+        correctAnswer: "d"
+    },
+    {
+        question: "Hasil konversi dari bilangan heksadesimal A6 ke desimal adalah....",
+        answers: {
+            a: "100",
+            b: "160",
+            c: "162",
+            d: "165",
+            e: "166"
+        },
+        code : 8,
+        correctAnswer: "e"
+    },
+    {
+        question: "Hasil konversi dari bilangan oktal 22 ke desimal adalah....",
+        answers: {
+            a: "10",
+            b: "11",
+            c: "12",
+            d: "18",
+            e: "21"
+        },
+        code : 9,
+        correctAnswer: "d"
+    },
+    {
+        question: "Hasil konversi dari bilangan oktal 76 ke heksadesimal adalah....",
+        answers: {
+            a: "33",
+            b: "34",
+            c: "3C",
+            d: "3D",
+            e: "3E" //terganti
+        },
+        code : 10,
+        correctAnswer: "e" 
+    },
+    {
+        question: "Bentuk sandi BCD dari bilangan desimal 776 adalah....",
+        answers: {
+            a: "0111 0111 0101",
+            b: "0111 0111 0110",
+            c: "1111 1111 0110",
+            d: "1111 1111 1111",
+            e: "0111 1100 0011"
+        },
+        code : 11,
+        correctAnswer: "b"
+    },
+    {
+        question: "Bentuk sandi BCH dari bilangan heksadesimal A5 adalah...",
+        answers: {
+            a: "1010 0101",
+            b: "1110 1010",
+            c: "1010 0011",
+            d: "0101 1010",
+            e: "1010 1111"
+        },
+        code : 12,
+        correctAnswer: "a"
+    },
+    {
+        question: "Gunakan tabel di bawah ini, kemudian ubahlah bilangan ASCII @AbC&$ ke dalam bentuk heksadesimal. Jawaban yang paling benar untuk hasil penyandian tersebut adalah....<br><img src='/img/ascii.png' class='img-fluid'>",
+        answers: {
+            a: "40 41 4B 47 99 21",
+            b: "40 41 62 43 26 21",
+            c: "40 41 62 63 27 24",
+            d: "40 41 62 43 26 24",
+            e: "40 41 42 43 44 24"
+        },
+        code : 13,
+        correctAnswer: "d"
+    },
+    {
+        question: "Kepanjangan dari ASCII adalah....",
+        answers: {
+            a: "American Step Code In International",
+            b: "American Standard Code for Information Interchange",
+            c: "American Standard Code for International Information",
+            d: "Australian Status Code for Interest Information",
+            e: "American Standard Code for Internet Interchange"
+        },
+        code : 14,
+        correctAnswer: "b"
+    },
+    {
+        question: "Makna dari BCH adalah....",
+        answers: {
+            a: "Heksadesimal yang dikodekan dalam bentuk biner",
+            b: "Kode biner yang dikodekan dalam tabel",
+            c: "Bilangan heksadesimal pada urutan biner",
+            d: "Biner yang dikodekan dalam desimal",
+            e: "Heksadesimal untuk bilangan lain"
+        },
+        code : 15,
+        correctAnswer: "a"
+    }
+]
